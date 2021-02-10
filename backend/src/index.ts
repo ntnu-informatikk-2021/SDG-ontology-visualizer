a188edebc857bffb7cb930f8863e263bb945ae43
AndreasWintherMoen <andreas@winthermoen.no>
2021-02-10 13:10:15 +0100

    Add body-parser

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import config from './config';
 import routes from './routes';
 import databaseLogin from './database/login';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 app.use('/api', routes);
 
 const init = async (): Promise<void> => {
