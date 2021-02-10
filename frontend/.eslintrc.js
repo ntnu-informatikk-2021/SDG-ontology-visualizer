cc01bc10d2e234d1f0631ba31be725832bd22a2f
AndreasWintherMoen <andreas@winthermoen.no>
2021-02-10 02:37:55 +0100

    Add eslint exceptions to make demo app work

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,5 +20,9 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'no-console': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
 };
