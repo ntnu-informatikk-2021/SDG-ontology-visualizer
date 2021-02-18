5dc20870341b6f9bd91bb8c5a2a7b1cfd86af05e
Erik Skår <erikmsk@stud.ntnu.no>
2021-02-18 11:56:19 +0100

    Add chakra ui and implement it in table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Box, ChakraProvider } from '@chakra-ui/react';
 import OntologyTable from './components/OntologyTable';
 
 const App = () => (
-  <div className="App">
-    <OntologyTable />
-  </div>
+  <ChakraProvider>
+    <Box h="100vh">
+      <div className="App">
+        <OntologyTable />
+      </div>
+    </Box>
+  </ChakraProvider>
 );
 
 export default App;
