f81ccb965cc225a8065f6ba7fd9a012d7e931d73
yvindo <oyvinjs@stud.ntnu.no>
2021-03-15 18:10:48 +0100

    added function in api module connecting to the subclasses endpoint

--- frontend/src/api/ontologies.ts
+++ frontend/src/api/ontologies.ts
@@ -3,3 +3,6 @@ import api from './api';
 // eslint-disable-next-line import/prefer-default-export
 export const getRelations = (classId: string): Promise<any> =>
   api.GET(`ontologies/relations/${encodeURIComponent(classId)}`);
+
+export const getSubclasses = (classId: string): Promise<any> =>
+  api.GET(`ontologies/subclasses/${encodeURIComponent(classId)}`);
