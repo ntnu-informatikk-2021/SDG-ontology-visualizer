701e2248445679c411abfb5d9dd2fc27afe965d5
Erik Mjaaland Skår <43439578+erikskaar@users.noreply.github.com>
2021-03-16 12:28:23 +0100

    Add endpoint in frontend and backend for retrieving description (#47)

--- frontend/src/api/ontologies.ts
+++ frontend/src/api/ontologies.ts
@@ -3,3 +3,6 @@ import api from './api';
 // eslint-disable-next-line import/prefer-default-export
 export const getRelations = (classId: string): Promise<any> =>
   api.GET(`ontologies/relations/${encodeURIComponent(classId)}`);
+
+export const getDescription = (classId: string): Promise<any> =>
+  api.GET(`ontologies/description/${encodeURIComponent(classId)}`);
