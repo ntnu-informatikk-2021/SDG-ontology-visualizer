c1ddf9b98aa312db7c3e58136c1885fa14b03807
Benedicte Helen Myrvoll <benedihm@stud.ntnu.no>
2021-03-16 13:40:54 +0100

    Fix unused import

--- backend/src/database/getAnnotations.ts
+++ backend/src/database/getAnnotations.ts
@@ -1,7 +1,6 @@
 import DB from './index';
 import { OntologyEntity } from '../types/types';
 import getAnnotations from './queries/getAnnotations';
-import { mapIdToOntologyEntity } from '../common/database';
 
 export default async (classId: string): Promise<Array<OntologyEntity>> => {
   const query = getAnnotations(classId);
