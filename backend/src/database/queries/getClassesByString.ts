26f6abed6b5084bd36583875cfe30e73ecd0bf52
AndreasWintherMoen <andreas@winthermoen.no>
2021-03-19 17:36:03 +0100

    Add label to search results

--- backend/src/database/queries/getClassesByString.ts
+++ backend/src/database/queries/getClassesByString.ts
@@ -6,16 +6,18 @@ export default (searchTerm: string, limitResults?: number): string => {
 
   return `
     ${prefixes}
-    SELECT DISTINCT ?Subject
+    SELECT DISTINCT ?Subject ?SubjectLabel
     WHERE { 
         {
           ?Subject rdfs:label ?o .
           FILTER regex(?o, "${searchTerm}", "i") .
+          OPTIONAL {?Subject rdfs:label ?SubjectLabel}
         }
         UNION
         {
           ?Subject ?p ?o .
-          FILTER regex(str(?Subject), "${searchTerm}", "i")
+          FILTER regex(str(?Subject), "${searchTerm}", "i") .
+          OPTIONAL {?Subject rdfs:label ?SubjectLabel}
         }
       
     } LIMIT ${limitResults ? Math.min(limitResults, 200) : 20}
