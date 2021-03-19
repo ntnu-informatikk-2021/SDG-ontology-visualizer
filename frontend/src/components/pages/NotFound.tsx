2fc9d18229f68f1aeac260df4415f86de472b23d
AndreasWintherMoen <andreas@winthermoen.no>
2021-03-19 13:47:56 +0100

    Style not found page

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -1,9 +1,39 @@
+import { ArrowLeftIcon } from '@chakra-ui/icons';
+import { Box, Button, Center, Heading, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound: React.FC = () => (
-  <div>
-    <p>You fucked up son!</p>
-  </div>
+  <Center>
+    <Box
+      h="container.sm"
+      w="container.md"
+      mt="32"
+      display="flex"
+      flexDirection="column"
+      alignContent="center"
+      justifyContent="space-between"
+      bg="red.200"
+      borderRadius="3xl"
+      backgroundImage="url('https://media.istockphoto.com/vectors/error-page-or-file-not-found-icon-vector-id924949200?b=1&k=6&m=924949200&s=612x612&w=0&h=eonpW4ymWtCIKLt8k85EhAZ32kiJ-v_LmTEkUcPmg7Q=')"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+    >
+      <Box display="flex" flexDirection="column">
+        <Heading as="h2" mt="4">
+          404 Not Found
+        </Heading>
+        <Text>This page does not exist</Text>
+      </Box>
+      <Link to="/">
+        <Button w="100%" colorScheme="blue" mr={3}>
+          <ArrowLeftIcon mr="2" />
+          Back to Home Page
+        </Button>
+      </Link>
+    </Box>
+  </Center>
 );
 
 export default NotFound;
