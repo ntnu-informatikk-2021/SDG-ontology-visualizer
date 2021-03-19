2b736f66ec885cb12362a7049c006e3dd6138334
Andreas Winther Moen <30579542+AndreasWintherMoen@users.noreply.github.com>
2021-03-19 14:15:18 +0100

    Fix navbar routing (#66)

--- frontend/src/components/atoms/Navbar.tsx
+++ frontend/src/components/atoms/Navbar.tsx
@@ -1,25 +1,21 @@
 import { InfoIcon } from '@chakra-ui/icons';
 import { Box, Button, Flex, Heading, Spacer } from '@chakra-ui/react';
 import React from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <Flex bgGradient="linear(to-r, green.200, pink.500)" w="100%" p={4} color="white">
     <Box>
-      <Router>
-        <Link to="/">
-          <Heading>Trondheim SDG Ontology club</Heading>
-        </Link>
-      </Router>
+      <Link to="/">
+        <Heading>Trondheim SDG Ontology club</Heading>
+      </Link>
     </Box>
     <Spacer />
-    <Router>
-      <Link to="/about">
-        <Button leftIcon={<InfoIcon />} colorScheme="teal" variant="solid">
-          About
-        </Button>
-      </Link>
-    </Router>
+    <Link to="/about">
+      <Button leftIcon={<InfoIcon />} colorScheme="teal" variant="solid">
+        About
+      </Button>
+    </Link>
   </Flex>
 );
 
