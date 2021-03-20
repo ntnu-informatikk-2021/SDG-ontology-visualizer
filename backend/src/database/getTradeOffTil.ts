2de4c51cab4ec7a4d53e801fe6c007239691fa75
Kapteinkrok <61871576+Kapteinkrok@users.noreply.github.com>
2021-03-20 13:36:47 +0100

    Implemented changes accoring to issue: #72 (#73)
    Co-authored-by: Per Solibakke <persol@stud.ntnu.no>

--- backend/src/database/getTradeOffTil.ts
+++ backend/src/database/getTradeOffTil.ts
@@ -1,10 +1,10 @@
 import { ApiError } from '../types/types';
 import { mapRecordToObject } from '../common/database';
 import DB from './index';
-import getConnectionsSDGAndTBL from './queries/getConnectionsSDGAndTBL';
+import getTradeOff from './queries/getTradeOff';
 
 export default async (nodeId: string): Promise<Array<Node>> => {
-  const query = getConnectionsSDGAndTBL(nodeId);
+  const query = getTradeOff(nodeId);
   if (!nodeId) {
     throw new ApiError(400, 'Could not parse ontology entity from the given class ID');
   }
--- backend/src/routes/ontologies.ts
+++ backend/src/routes/ontologies.ts
@@ -3,7 +3,9 @@ import getSubclasses from '../database/getSubclasses';
 import getRelations from '../database/getRelations';
 import getAnnotations from '../database/getAnnotations';
 import getSustainabilityGoals from '../database/getSustainabilityGoals';
-import getConnectionsSDGAndTBL from '../database/getConnectionsSDGAndTBL';
+import getContributions from '../database/getContributions';
+import getTradeOff from '../database/getTradeOffTil';
+import getDevelopmentArea from '../database/getDevelopmentArea';
 import onError from './middleware/onError';
 
 const router = Router();
@@ -44,9 +46,27 @@ const getSustainabilityGoalsFromOntology = async (req, res) => {
   }
 };
 
-const getSDGTBLConnections = async (req, res) => {
+const getContributionsToNodes = async (req, res) => {
   try {
-    const data = await getConnectionsSDGAndTBL(req.params.classId);
+    const data = await getContributions(req.params.classId);
+    res.json(data);
+  } catch (e) {
+    onError(e, req, res);
+  }
+};
+
+const getTradeOffToNodes = async (req, res) => {
+  try {
+    const data = await getTradeOff(req.params.classId);
+    res.json(data);
+  } catch (e) {
+    onError(e, req, res);
+  }
+};
+
+const getDevelopmentAreaToNodes = async (req, res) => {
+  try {
+    const data = await getDevelopmentArea(req.params.classId);
     res.json(data);
   } catch (e) {
     onError(e, req, res);
@@ -57,6 +77,8 @@ router.get('/relations/:classId', getRelationsFromClass);
 router.get('/subclasses/:classId', getSubclassesFromClass);
 router.get('/annotations/:classId', getAnnotationsFromClass);
 router.get('/sustainabilityGoals', getSustainabilityGoalsFromOntology);
-router.get('/contributions/:classId', getSDGTBLConnections);
+router.get('/contributions/:classId', getContributionsToNodes);
+router.get('/tradeoff/:classId', getTradeOffToNodes);
+router.get('/developmentarea/:classId', getDevelopmentAreaToNodes);
 
 export default router;
