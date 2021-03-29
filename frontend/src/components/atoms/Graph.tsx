1b914ae881fa96c4d25f210b6a07116c78e2366b
AndreasWintherMoen <andreas@winthermoen.no>
2021-03-30 00:45:01 +0200

    Add GraphSimulation.ts and move D3 logic from Graph.tsx

--- frontend/src/components/atoms/Graph.tsx
+++ frontend/src/components/atoms/Graph.tsx
@@ -1,191 +1,50 @@
 import { Center } from '@chakra-ui/react';
-import { D3ZoomEvent, select, Simulation, zoom } from 'd3';
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRelations } from '../../api/ontologies';
-import {
-  makePredicateUnique,
-  mapNodeToGraphNode,
-  mapOntologyToGraphEdge,
-  removeDuplicates,
-} from '../../common/d3';
-import {
-  createForceSimulation,
-  drawLinks,
-  drawNodeLabels,
-  drawNodes,
-  resetSimulation,
-  updateLabelPositions,
-  updateLinkPositions,
-  updateNodePositions,
-} from '../../d3/d3';
+import GraphSimulation from '../../d3/GraphSimulation';
 import useWindowDimensions from '../../hooks/useWindowsDimensions';
 import { setError } from '../../state/reducers/apiErrorReducer';
 import { selectNode } from '../../state/reducers/ontologyReducer';
 import { RootState } from '../../state/store';
-import { D3Edge, GraphEdge, GraphNode, Ontology } from '../../types/ontologyTypes';
+import { GraphNode } from '../../types/ontologyTypes';
 
 const Graph: React.FC = () => {
   const { height, width } = useWindowDimensions();
-  const [hasInitialized, setHasInitialized] = useState<boolean>(false);
-  const svgref = useRef<SVGSVGElement>(null);
-  const linksRef = useRef<SVGGElement>(null);
-  const nodesRef = useRef<SVGGElement>(null);
-  const edgeLabelsRef = useRef<SVGGElement>(null);
-  const nodeLabelsRef = useRef<SVGGElement>(null);
-  const [nodes, setNodes] = useState<GraphNode[]>([]);
-  const [links, setLinks] = useState<Array<GraphEdge | D3Edge>>([]);
-  const [forceSim, setForceSim] = useState<Simulation<GraphNode, GraphEdge>>();
-
+  const svgRef = useRef<SVGSVGElement>(null);
   const selectedNode = useSelector((state: RootState) => state.ontology.selectedNode);
-
   const dispatch = useDispatch();
-
-  const updateNodes = (ontologies: Array<Ontology>, clickedNode: GraphNode) => {
-    let newNodes = ontologies.map((ontology) =>
-      ontology.Subject.id === clickedNode.id ? ontology.Object : ontology.Subject,
-    );
-
-    if (hasInitialized) {
-      newNodes = nodes.concat(newNodes);
-    } else {
-      newNodes.push(clickedNode as GraphNode);
-    }
-    newNodes = newNodes
-      .filter(removeDuplicates)
-      .map((node) => mapNodeToGraphNode(node, clickedNode));
-
-    setNodes(newNodes);
-  };
-
-  const updateLinks = (ontologies: Array<Ontology>) => {
-    let newLinks: Array<GraphEdge | D3Edge> = ontologies
-      .map(makePredicateUnique)
-      .map(mapOntologyToGraphEdge);
-    if (hasInitialized) {
-      newLinks = links.concat(newLinks);
-    }
-    newLinks = newLinks.filter(removeDuplicates);
-
-    console.log(newLinks.map((link) => link.source));
-    setLinks(newLinks);
-  };
+  const [simulation, setSimulation] = useState<GraphSimulation>();
 
   const loadData = async () => {
     if (!selectedNode) {
       dispatch(setError(new Error('No nodes selected in Graph')));
       return;
     }
-    if (!hasInitialized) {
-      setHasInitialized(true);
-    }
+    if (!simulation) return;
 
     const ontologies = await getRelations(selectedNode.id);
-    if (ontologies.length === 0) {
-      if (!hasInitialized) {
-        setHasInitialized(false);
-      }
-      return;
-    }
-
-    updateNodes(ontologies, selectedNode);
-    updateLinks(ontologies);
+    simulation.addData(ontologies, selectedNode);
   };
 
-  const onClickNode = (node: GraphNode) => {
+  const onClickNode = (node: GraphNode): void => {
     dispatch(selectNode(node));
   };
 
-  const drawGraph = () => {
-    if (!nodes || !links || nodes.length === 0 || links.length === 0) return;
-
-    // const svg = select(svgref.current);
-    const svgLinks = select(linksRef.current);
-    const svgNodes = select(nodesRef.current);
-    // const svgLinkLabels = select(linksRef.current);
-    const svgNodeLabels = select(nodeLabelsRef.current);
-
-    drawLinks(svgLinks, links, '.link', 'none', '#aaa', 1);
-    drawNodes(
-      svgLinks,
-      svgNodes,
-      nodes,
-      '.node',
-      10,
-      '#4299e1',
-      '#322659',
-      onClickNode,
-      links,
-      forceSim,
-    );
-    drawNodeLabels(svgNodeLabels, nodes, '.nodeLabel');
-    // drawEdgeLabels(svgLinkLabels, links, '.edgeLabel');
-
-    // TODO: Simplify this
-    if (forceSim) {
-      forceSim.on('tick', () => {
-        updateLinkPositions(svgLinks, links, '.link');
-        updateNodePositions(svgNodes, nodes, '.node');
-        updateLabelPositions(svgNodeLabels, nodes, '.nodeLabel');
-        // updateEdgeLabelPositions(svgLinkLabels, links, '.edgeLabel');
-      });
-      resetSimulation(forceSim, nodes, links);
+  useEffect(() => {
+    if (!svgRef || !svgRef.current || !selectedNode) return;
+    if (!simulation) {
+      setSimulation(new GraphSimulation(svgRef.current, width, height, selectedNode, onClickNode));
     } else {
-      const newForceSim = createForceSimulation(nodes, links, width, height - 200).on(
-        'tick',
-        () => {
-          // console.log(newForceSim.alpha());
-          updateLinkPositions(svgLinks, links, '.link');
-          updateNodePositions(svgNodes, nodes, '.node');
-          updateLabelPositions(svgNodeLabels, nodes, '.nodeLabel');
-          // updateEdgeLabelPositions(svgLinkLabels, links, '.edgeLabel');
-        },
-      );
-      setForceSim(newForceSim);
+      loadData();
     }
-  };
-
-  useEffect(() => {
-    loadData();
-  }, [selectedNode]);
-
-  useEffect(() => {
-    console.log('nodes or links changed');
-    drawGraph();
-  }, [links]);
-
-  // Does only change the transform values of each g element.
-  useEffect(() => {
-    const svg = select(svgref.current);
-    const svgLinks = select(linksRef.current);
-    const svgNodes = select(nodesRef.current);
-    const svgLinkLabels = select(edgeLabelsRef.current);
-    const svgNodeLabels = select(nodeLabelsRef.current);
-
-    svg.call(
-      zoom()
-        .on('zoom', (event: D3ZoomEvent<SVGSVGElement, any>) => {
-          const scale = event.transform.k;
-          const translate = [event.transform.x, event.transform.y];
-          svgLinks.attr('transform', `translate(${translate}) scale(${scale})`);
-          svgNodes.attr('transform', `translate(${translate}) scale(${scale})`);
-          svgLinkLabels.attr('transform', `translate(${translate}) scale(${scale})`);
-          svgNodeLabels.attr('transform', `translate(${translate}) scale(${scale})`);
-        })
-        .scaleExtent([0.2, 4]) as any,
-    );
-  }, []);
+  }, [selectedNode, svgRef, simulation]);
 
   return (
     <Center mx="auto" my="0">
-      <svg id="svgGraph" height={height - 200} width={width} ref={svgref}>
-        <g ref={linksRef} />
-        <g ref={nodesRef} />
-        <g ref={edgeLabelsRef} />
-        <g ref={nodeLabelsRef} />
-      </svg>
+      <svg id="svgGraph" height={height - 200} width={width} ref={svgRef} />
     </Center>
   );
 };
-Graph.defaultProps = { initialNode: undefined };
+
 export default Graph;
---
+++ frontend/src/types/d3/svg.ts
@@ -0,0 +1,3 @@
+export type MainSvgSelection = d3.Selection<SVGSVGElement, unknown, null, undefined>;
+
+export type SubSvgSelection = d3.Selection<SVGGElement, unknown, null, undefined>;
