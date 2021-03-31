ea5654f1bf18f80f67f72e13378206871f19b93d
AndreasWintherMoen <andreas@winthermoen.no>
2021-03-31 14:51:31 +0200

    Store login status in database/login

--- backend/src/database/login.ts
+++ backend/src/database/login.ts
@@ -1,12 +1,22 @@
+import { LoginResponse } from '@innotrade/enapso-graphdb-client';
 import DB from './index';
 import config from '../config';
 
-export default (): void => {
-  DB.login(config.GRAPHDB_USERNAME, config.GRAPHDB_PASSWORD)
-    .then((result: any) => {
-      console.log(result);
+let isLoggedIn = false;
+
+export const getLoginStatus = () => isLoggedIn;
+
+export default async (): Promise<void> => {
+  console.log('Connecting to database...');
+  await DB.login(config.GRAPHDB_USERNAME, config.GRAPHDB_PASSWORD)
+    .then((result: LoginResponse) => {
+      console.log(`Connected to database. Response: ${result.statusCode} ${result.message}`);
+      isLoggedIn = true;
     })
-    .catch((err: any) => {
-      console.log(err);
+    .catch((err: LoginResponse) => {
+      console.log(
+        `Could not connect to database (status code ${err.statusCode}). Message: ${err.message}`,
+      );
+      isLoggedIn = false;
     });
 };
