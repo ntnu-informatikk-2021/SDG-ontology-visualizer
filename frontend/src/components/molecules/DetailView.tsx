0631413c1d371d4c988606227e02d3dd716ee267
AndreasWintherMoen <andreas@winthermoen.no>
2021-04-08 16:10:38 +0200

    Make more information link clickable

--- frontend/src/components/molecules/DetailView.tsx
+++ frontend/src/components/molecules/DetailView.tsx
@@ -1,5 +1,5 @@
 import { ArrowForwardIcon } from '@chakra-ui/icons';
-import { Box, Button, ButtonGroup, Flex, Heading, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, Flex, Heading, Link, Stack, Text } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -66,11 +66,15 @@ const DetailView: React.FC = () => {
             <Text fontSize="xl" mt="2">
               {annotations.description}
             </Text>
-            <Text fontSize="base" mt="2">
-              {annotations.moreInformation
-                ? ['Mer informasjon finnes her:  ', annotations.moreInformation]
-                : ''}
-            </Text>
+            {annotations.moreInformation && (
+              <Text fontSize="base" mt="2">
+                Mer informasjon finnes her:
+                {'  '}
+                <Link href={annotations.moreInformation} isExternal fontWeight="bold">
+                  {annotations.moreInformation}
+                </Link>
+              </Text>
+            )}
           </>
         </SlideInDrawer>
         <ContextDivider visible={!expanded} />
