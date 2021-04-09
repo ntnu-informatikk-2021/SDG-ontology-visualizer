2aa36700e54e879de4f3ca3622688b94696c0164
Kapteinkrok <61871576+Kapteinkrok@users.noreply.github.com>
2021-04-09 12:45:23 +0200

    Moved type and correlation from Ontologyentity to Node (#144)
    * Moved type and correlation from Ontologyentity to Node
    
    * Changed a variable name from uppercase to lower case
    
    * Removed the filtering of hasWineDescriptor in getRelations as this is not nessecary
    
    * Update getRelations.ts
    
    Co-authored-by: Per Solibakke <persol@stud.ntnu.no>
    Co-authored-by: Vemund Eggemoen <49310062+vemund0507@users.noreply.github.com>

--- backend/src/database/getRelations.ts
+++ backend/src/database/getRelations.ts
@@ -5,29 +5,28 @@ import getRelations from './queries/getRelations';
 import {
   addEntityToNullFields,
   isNotLoopOntology,
-  mapIdToOntologyEntity,
+  mapIdToNode,
   mapRecordToOntology,
 } from '../common/database';
 
 const isRelevantOntology = (ontology: Ontology): boolean => {
   if (!ontology || !ontology.Predicate || !(ontology.Subject || ontology.Object)) return false;
   if (ontology.Predicate.id.includes('#type')) return false;
-  const ontologyEntity = ontology.Subject || ontology.Object;
-  if (!ontologyEntity || ontologyEntity.id.includes('node')) return false;
-  if (ontology.Predicate.id.includes('hasWineDescriptor')) return false;
+  const node = ontology.Subject || ontology.Object;
+  if (!node || node.id.includes('node')) return false;
   return true;
 };
 
 export default async (classId: string): Promise<Array<Ontology>> => {
-  const ontologyEntity = mapIdToOntologyEntity(classId);
-  if (!ontologyEntity) {
-    throw new ApiError(400, 'Could not parse ontology entity from the given class ID');
+  const node = mapIdToNode(classId);
+  if (!node) {
+    throw new ApiError(400, 'Could not parse node from the given class ID');
   }
   const query = getRelations(classId);
   const response = await DB.query(query, { transform: 'toJSON' });
   const ontologies = response.records
     .map(mapRecordToOntology)
-    .map((ont) => addEntityToNullFields(ont, ontologyEntity))
+    .map((ont) => addEntityToNullFields(ont, node))
     .filter(isRelevantOntology)
     .filter(isNotLoopOntology);
 
