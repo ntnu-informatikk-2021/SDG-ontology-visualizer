a7379a371c2a9d514eef6dfd9b74d7dcaa98c2d2
AndreasWintherMoen <andreas@winthermoen.no>
2021-04-15 12:48:33 +0200

    Fix merge main test issues

--- frontend/src/tests/snapshotTesting/atoms/GraphSidebar.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-// eslint-disable-next-line import/extensions
-import GraphSidebar from '../../../components/atoms/GraphSidebar';
-
-it('renders when there are no items', () => {
-  const tree = renderer.create(<GraphSidebar onSubgoalFilter={() => {}} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
