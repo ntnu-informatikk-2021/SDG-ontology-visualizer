1853f0c40fccbbe97701f1bbe6bfee3cab15eea8
AndreasWintherMoen <andreas@winthermoen.no>
2021-04-15 12:33:08 +0200

    Remove unnecessary console log in test

--- frontend/src/tests/snapshotTesting/atoms/GraphSidebar.test.tsx
+++ frontend/src/tests/snapshotTesting/atoms/GraphSidebar.test.tsx
@@ -2,19 +2,7 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import GraphSidebar from '../../../components/atoms/GraphSidebar';
 
-const sidebar = {
-  id: 't2',
-};
-
 it('renders when there are no items', () => {
-  const tree = renderer
-    .create(
-      <GraphSidebar
-        onSubgoalFilter={() => {
-          console.log(sidebar);
-        }}
-      />,
-    )
-    .toJSON();
+  const tree = renderer.create(<GraphSidebar onSubgoalFilter={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
