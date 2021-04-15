48dae9b1c9527ab0d4a2f1403ab8026225c83650
AndreasWintherMoen <andreas@winthermoen.no>
2021-04-15 12:39:19 +0200

    Add ESLint exception for false positive CI

--- frontend/src/tests/snapshotTesting/atoms/GraphSidebar.test.tsx
+++ frontend/src/tests/snapshotTesting/atoms/GraphSidebar.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+// eslint-disable-next-line import/extensions
 import GraphSidebar from '../../../components/atoms/GraphSidebar';
 
 it('renders when there are no items', () => {
